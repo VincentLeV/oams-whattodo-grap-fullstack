bd263f28df4c83687bb2bc76fa58c317bdd345e5
Vincent Le <vincentle.fh@gmail.com>
2023-01-01 14:07:27 +0200

    Minor fix

--- client/src/graphql-client.js
+++ client/src/graphql-client.js
@@ -4,8 +4,8 @@ import {
     HttpLink
 } from "@apollo/client"
 
-// const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-backend.onrender.com/graphql" 
-const URI = "https://oams-whattodo-grap-backend.onrender.com/graphql" 
+const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-backend.onrender.com/graphql" 
+
 const httpLink = new HttpLink({ 
     uri: URI
 })
