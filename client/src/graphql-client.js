47db92b35daae3d46623d58afa97a802fa3a63f8
Vincent Le <vincentle.fh@gmail.com>
2023-01-01 14:06:50 +0200

    Migrated to Render

--- client/src/graphql-client.js
+++ client/src/graphql-client.js
@@ -4,9 +4,8 @@ import {
     HttpLink
 } from "@apollo/client"
 
-const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap.herokuapp.com/graphql" 
-// const URI = "https://oams-whattodo-grap.herokuapp.com/graphql" 
-
+// const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-backend.onrender.com/graphql" 
+const URI = "https://oams-whattodo-grap-backend.onrender.com/graphql" 
 const httpLink = new HttpLink({ 
     uri: URI
 })
