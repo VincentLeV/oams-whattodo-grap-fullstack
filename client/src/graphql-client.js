af3e3333b6128c4dfe5a61707fa9aa6d7ef9729f
Vincent Le <vincentle.fh@gmail.com>
2023-01-01 16:05:00 +0200

    Migrated to Railway

--- client/src/graphql-client.js
+++ client/src/graphql-client.js
@@ -4,7 +4,8 @@ import {
     HttpLink
 } from "@apollo/client"
 
-const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-backend.onrender.com/graphql" 
+// const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-backend.onrender.com/graphql" 
+const URI = process.env.REACT_APP_BACKEND_BASE_URL || "https://oams-whattodo-grap-fullstack-production.up.railway.app/graphql" 
 
 const httpLink = new HttpLink({ 
     uri: URI
